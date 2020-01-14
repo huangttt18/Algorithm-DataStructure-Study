4a944127436afb6a43c6de557423780431a108d9
huangttt18 <danielhuang@gmail.com>
2020-01-14 15:43:25 +0800

    leetcode 1281

---
+++ src/main/java/com/study/practice/jan14/LeetCode1281.java
@@ -0,0 +1,48 @@
+package com.study.practice.jan14;
+
+/**
+ * 整数的各位积和之差
+ *
+ * @author Daniel 2020/1/14
+ */
+public class LeetCode1281 {
+
+    public static int subtractProductAndSum(int n) {
+        return plusOfNum(n) - sumOfNum(n);
+    }
+
+    /**
+     * 各位数之和
+     */
+    public static int sumOfNum(int num) {
+        int sum = 0;
+        while (true) {
+            sum += num % 10;
+            if (num / 10 == 0) {
+                break;
+            }
+            num /= 10;
+        }
+        return sum;
+    }
+
+    /**
+     * 各位数只积
+     */
+    public static int plusOfNum(int num) {
+        int plus = 1;
+        while (true) {
+            int remainder = num % 10;
+            plus *= remainder;
+            if (num / 10 == 0) {
+                break;
+            }
+            num /= 10;
+        }
+        return plus;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(subtractProductAndSum(4421));;
+    }
+}
